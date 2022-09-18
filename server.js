8e7245199cbfcbe650535e6f4df30b29c77685c4
Oma Withers <omacwitherssei@gmail.com>
2022-09-18 12:07:42 -0400

    "Updated Server.js"

--- server.js
+++ server.js
@@ -1,43 +1,108 @@
-//**DEPENDENCIES**
+// DEPENDENCIES
 const express = require("express");
 const app = express();
-const port = 3000
-const budget = require("./models/budget.js");
-//**DEPENDENCIES**
-
-//**ROUTES**
-app.use((req, res, next) => {
-  console.log("I run for all routes")
-  next()
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Product = require('./models/products');
+const methodOverride = require("method-override")
+​
+// DATABASE CONFIGURATION
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-
-app.use(express.urlencoded({ extended: false }))
-
-app.get("/budgets", (req, res) => {
-  res.render("index.ejs", {
-    allBudgets: budget
-  });
+​
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+​
+// MIDDLEWARE  & BODY PARSER
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"))
+app.use(express.static(__dirname + '/public'));
+​
+// ROUTES
+//SEED
+const productsSeed = require("./models/productsSeed.js");
+​
+app.get('/products/seed', (req, res) => {
+    Product.deleteMany({}, (error, allProducts) => { });
+​
+    Product.create(productsSeed, (error, data) => {
+        res.redirect("/products")
+    })
 });
-
-app.get("/budgets/new", (req,res) =>{
-  res.render("new.ejs")
+​
+// INDEX
+app.get('/products', (req, res) => {
+    Product.find({}, (error, allProducts) => {
+        res.render('index.ejs', {
+            products: allProducts
+        })
+    })
 });
-
-app.post("/budgets", (req, res)=>{
-budget.push(req.body)
-res.redirect("/budgets")
+​
+// NEW
+app.get("/products/new", (req, res) => {
+    res.render('new.ejs')
 });
-
-app.get("/budgets/:index", (req, res)=>{
-  res.render("show.ejs", {
-    budgets: budgets[req.params.index]
-  });
-});
-
-app.get("/", (req, res) =>{
-  res.send("Welcome to the Budgtr App!");
-});
-
-app.listen(port, () =>{
-  console.log("express is listening")
+​
+// DELETE
+app.delete("/products/:id", (req, res) => {
+    Product.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect("/products")
+    })
 })
+​
+// UPDATE
+app.put("/products/:id", (req, res) => {
+    if(req.body.buy <=  0){ 
+        req.body.buy = ('OUT OF STOCK'); 
+    } else { 
+         req.body.buy = (`${req.body.buy -=1}`);
+        }
+    Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+        },
+        (error, updatedProduct) => {
+            res.redirect(`/products/${req.params.id}`)
+        }
+    )
+})
+​
+// CREATE
+app.post('/products', (req, res) => {
+    Product.create(req.body, (error, createdProduct) => {
+        res.redirect("/products");
+    });
+});
+​
+// EDIT
+app.get("/products/:id/edit", (req, res) => {
+    Product.findById(req.params.id, (error, foundProduct) => {
+        res.render("edit.ejs", {
+            product: foundProduct,
+        })
+    })
+})
+​
+// SHOW
+app.get('/products/:id', (req, res) => {
+    Product.findById(req.params.id, (err, foundProduct) => {
+        res.render("show.ejs", {
+            product: foundProduct
+        })
+    });
+});
+​
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`express is listening on port: ${PORT}`)
+});
